ed8322ae8caee6ed357dc73a5eaa5bdde6f62668
Daniel Hill <dahi2387@colorado.edu>
2024-11-07 15:43:41 -0700

    pre week 2 meeting commit

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -1,39 +1,49 @@
-// ----------------------------------   DEPENDENCIES  ----------------------------------------------
-const express = require('express');
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
+const express = require('express'); // To build an application server or API
 const app = express();
 const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
 const path = require('path');
-const pgp = require('pg-promise')();
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 const bodyParser = require('body-parser');
-const session = require('express-session');
+const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
+const bcrypt = require('bcryptjs'); //  To hash passwords
+const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
 
-// -------------------------------------  APP CONFIG   ----------------------------------------------
+// *****************************************************
+// <!-- Section 2 : Connect to DB -->
+// *****************************************************
 
 // create `ExpressHandlebars` instance and configure the layouts and partials dir.
 const hbs = handlebars.create({
   extname: 'hbs',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname + '/views/partials',
+  layoutsDir: __dirname + '/ProjectSourceCode/src/views/layouts',
+  partialsDir: __dirname + '/ProjectSourceCode/src/views/partials',
 });
 
-// Register `hbs` as our view engine using its bound `engine()` function.
-app.engine('hbs', hbs.engine);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(bodyParser.json());
-// set Session
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: true,
-    resave: true,
+// database configuration
+const dbConfig = {
+  host: 'db', // the database server
+  port: 5432, // the database port
+  database: process.env.POSTGRES_DB, // the database name
+  user: process.env.POSTGRES_USER, // the user account to connect with
+  password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+// test your database
+db.connect()
+  .then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
+    obj.done(); // success, release the connection;
   })
-);
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+  .catch(error => {
+    console.log('ERROR:', error.message || error);
+  });
 
 // -------------------------------------  DB CONFIG AND CONNECT   ---------------------------------------
 const dbConfig = {
@@ -59,10 +69,13 @@ db.connect()
 // -------------------------------------  ROUTES for home.hbs   ----------------------------------------------
 
 app.get('/', (req, res) => {
-  res.render('pages/home');
+  res.render('pages/register');
 });
 
-// -------------------------------------  START THE SERVER   ----------------------------------------------
+// *****************************************************
+// <!-- Section 5 : Start Server-->
+// *****************************************************
+// starting the server and keeping the connection open to listen for more requests
 
 app.listen(3000);
 console.log('Server is listening on port 3000');
