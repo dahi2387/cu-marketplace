4e0d09cbe83fc5966f1ac299d4082de06d9c53aa
Amir Mhamdi <10amirmhamdi@gmail.com>
2024-11-13 12:08:59 -0700

    Added login API test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -60,5 +60,32 @@ describe('Server!', () => {
         done();
       });
   });
+});
+
+//Test cases for Login
+describe('Testing Login API', () => {
+  it('positive : /login', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({email: 'abc@colorado.edu', password: '456'})
+      .end((err, res) => {
+        expect(res).to.have.status('success');
+        expect(res.body.message).to.equals('Welcome!');
+        done();
+      });
+  });
+  //Negative test case
+  it('Negative : /login. Checking invalid login', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({email: 'xyz@gmail.com', password: '321'})
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+      });
+  });
 });*/
 // ********************************************************************************
